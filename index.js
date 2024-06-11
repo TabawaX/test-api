e0312a1c3851da422d5baf250f5567f86ae0783f
Tabawa Renki <112997721+TabawaX@users.noreply.github.com>
2024-06-11 13:19:45 +0700

    Update

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const cors = require("cors");
+const secure = require("ssl-express-www");
+
+const app = express();
+const __path = process.cwd(); 
+
+app.enable("trust proxy");
+app.set("json spaces", 2);
+app.use(cors());
+app.use(secure);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__path + "/sekai-page/home.html"); 
+});
+
+const router = require('./api/router');
+app.use('/api', router);
+
+module.exports = app;
