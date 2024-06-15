626025d9aa3b4cc4b08b883a1a548156fa02d683
Tabawa Renki <112997721+TabawaX@users.noreply.github.com>
2024-06-15 09:46:03 +0700

    Update router.js

--- api/router.js
+++ api/router.js
@@ -48,48 +48,52 @@ router.get("/pinterest", async (req, res) => {
         return res.status(403).json({ error: 'Invalid API key' });
     }
 
-    async function pinterest(query) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                const encodedQuery = encodeURIComponent(query);
-                const url = `https://id.pinterest.com/search/pins/?autologin=true&q=${encodedQuery}`;
 
-                const response = await axios.get(url, {
-                    headers: {
-                        "cookie": "_auth=1; _b=\"AVna7S1p7l1C5I9u0+nR3YzijpvXOPc6d09SyCzO+DcwpersQH36SmGiYfymBKhZcGg=\"; _pinterest_sess=TWc9PSZHamJOZ0JobUFiSEpSN3Z4a2NsMk9wZ3gxL1NSc2k2NkFLaUw5bVY5cXR5alZHR0gxY2h2MVZDZlNQalNpUUJFRVR5L3NlYy9JZkthekp3bHo5bXFuaFZzVHJFMnkrR3lTbm56U3YvQXBBTW96VUgzVUhuK1Z4VURGKzczUi9hNHdDeTJ5Y2pBTmxhc2owZ2hkSGlDemtUSnYvVXh5dDNkaDN3TjZCTk8ycTdHRHVsOFg2b2NQWCtpOWxqeDNjNkk3cS85MkhhSklSb0hwTnZvZVFyZmJEUllwbG9UVnpCYVNTRzZxOXNJcmduOVc4aURtM3NtRFo3STlmWjJvSjlWTU5ITzg0VUg1NGhOTEZzME9SNFNhVWJRWjRJK3pGMFA4Q3UvcHBnWHdaYXZpa2FUNkx6Z3RNQjEzTFJEOHZoaHRvazc1c1UrYlRuUmdKcDg3ZEY4cjNtZlBLRTRBZjNYK0lPTXZJTzQ5dU8ybDdVS015bWJKT0tjTWYyRlBzclpiamdsNmtpeUZnRjlwVGJXUmdOMXdTUkFHRWloVjBMR0JlTE5YcmhxVHdoNzFHbDZ0YmFHZ1VLQXU1QnpkM1FqUTNMTnhYb3VKeDVGbnhNSkdkNXFSMXQybjRGL3pyZXRLR0ZTc0xHZ0JvbTJCNnAzQzE0cW1WTndIK0trY05HV1gxS09NRktadnFCSDR2YzBoWmRiUGZiWXFQNjcwWmZhaDZQRm1UbzNxc21pV1p5WDlabm1UWGQzanc1SGlrZXB1bDVDWXQvUis3elN2SVFDbm1DSVE5Z0d4YW1sa2hsSkZJb1h0MTFpck5BdDR0d0lZOW1Pa2RDVzNySWpXWmUwOUFhQmFSVUpaOFQ3WlhOQldNMkExeDIvMjZHeXdnNjdMYWdiQUhUSEFBUlhUVTdBMThRRmh1ekJMYWZ2YTJkNlg0cmFCdnU2WEpwcXlPOVZYcGNhNkZDd051S3lGZmo0eHV0ZE42NW8xRm5aRWpoQnNKNnNlSGFad1MzOHNkdWtER0xQTFN5Z3lmRERsZnZWWE5CZEJneVRlMDd2VmNPMjloK0g5eCswZUVJTS9CRkFweHc5RUh6K1JocGN6clc1JmZtL3JhRE1sc0NMTFlpMVErRGtPcllvTGdldz0=; _ir=0"
-                    }
-                });
+async function pinterest(query) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const encodedQuery = encodeURIComponent(query);
+            const url = `https://id.pinterest.com/search/pins/?autologin=true&q=${encodedQuery}`;
 
-                const $ = cheerio.load(response.data);
-                const imageLinks = [];
-
-                $('div[data-test-id="pinWrapper"] img').each((index, element) => {
-                    const link = $(element).attr('src');
-                    if (link) {
-                        imageLinks.push(link.replace(/236x/g, '736x'));
-                    }
-                });
-
-                if (imageLinks.length > 0) {
-                    const shuffledImages = imageLinks.sort(() => 0.5 - Math.random()).slice(0, 50);
-                    resolve(shuffledImages);
-                } else {
-                    reject(new Error('No images found'));
+            const response = await axios.get(url, {
+                headers: {
+                    "cookie": "_auth=1; _b=\"AVna7S1p7l1C5I9u0+nR3YzijpvXOPc6d09SyCzO+DcwpersQH36SmGiYfymBKhZcGg=\"; _pinterest_sess=TWc9PSZHamJOZ0JobUFiSEpSN3Z4a2NsMk9wZ3gxL1NSc2k2NkFLaUw5bVY5cXR5alZHR0gxY2h2MVZDZlNQalNpUUJFRVR5L3NlYy9JZkthekp3bHo5bXFuaFZzVHJFMnkrR3lTbm56U3YvQXBBTW96VUgzVUhuK1Z4VURGKzczUi9hNHdDeTJ5Y2pBTmxhc2owZ2hkSGlDemtUSnYvVXh5dDNkaDN3TjZCTk8ycTdHRHVsOFg2b2NQWCtpOWxqeDNjNkk3cS85MkhhSklSb0hwTnZvZVFyZmJEUllwbG9UVnpCYVNTRzZxOXNJcmduOVc4aURtM3NtRFo3STlmWjJvSjlWTU5ITzg0VUg1NGhOTEZzME9SNFNhVWJRWjRJK3pGMFA4Q3UvcHBnWHdaYXZpa2FUNkx6Z3RNQjEzTFJEOHZoaHRvazc1c1UrYlRuUmdKcDg3ZEY4cjNtZlBLRTRBZjNYK0lPTXZJTzQ5dU8ybDdVS015bWJKT0tjTWYyRlBzclpiamdsNmtpeUZnRjlwVGJXUmdOMXdTUkFHRWloVjBMR0JlTE5YcmhxVHdoNzFHbDZ0YmFHZ1VLQXU1QnpkM1FqUTNMTnhYb3VKeDVGbnhNSkdkNXFSMXQybjRGL3pyZXRLR0ZTc0xHZ0JvbTJCNnAzQzE0cW1WTndIK0trY05HV1gxS09NRktadnFCSDR2YzBoWmRiUGZiWXFQNjcwWmZhaDZQRm1UbzNxc21pV1p5WDlabm1UWGQzanc1SGlrZXB1bDVDWXQvUis3elN2SVFDbm1DSVE5Z0d4YW1sa2hsSkZJb1h0MTFpck5BdDR0d0lZOW1Pa2RDVzNySWpXWmUwOUFhQmFSVUpaOFQ3WlhOQldNMkExeDIvMjZHeXdnNjdMYWdiQUhUSEFBUlhUVTdBMThRRmh1ekJMYWZ2YTJkNlg0cmFCdnU2WEpwcXlPOVZYcGNhNkZDd051S3lGZmo0eHV0ZE42NW8xRm5aRWpoQnNKNnNlSGFad1MzOHNkdWtER0xQTFN5Z3lmRERsZnZWWE5CZEJneVRlMDd2VmNPMjloK0g5eCswZUVJTS9CRkFweHc5RUh6K1JocGN6clc1JmZtL3JhRE1sc0NMTFlpMVErRGtPcllvTGdldz0=; _ir=0"
                 }
-            } catch (error) {
-                reject(error);
+            });
+
+            // Log HTML response for debugging
+            console.log(response.data);
+
+            const $ = cheerio.load(response.data);
+            const imageLinks = [];
+
+            $('div[data-test-id="pinWrapper"] img').each((index, element) => {
+                const link = $(element).attr('src');
+                if (link) {
+                    imageLinks.push(link.replace(/236x/g, '736x'));
+                }
+            });
+
+            if (imageLinks.length > 0) {
+                const shuffledImages = imageLinks.sort(() => 0.5 - Math.random()).slice(0, 50);
+                resolve(shuffledImages);
+            } else {
+                reject(new Error('No images found'));
             }
-        });
-    }
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
 
     try {
         const images = await pinterest(text);
         res.status(200).json({
             engineering: "Tabawa",
-            data: images
+            data: ${images}
         });
     } catch (error) {
-        console.error('Error in /tiktokdl endpoint:', error);
+        console.error('Error in /pinterest endpoint:', error);
         res.status(500).json({ error: error.message });
     }
 });
