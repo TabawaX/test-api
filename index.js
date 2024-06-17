6e732085fed794b04c69b4a76d9836ba202c069d
Tabawa Renki <112997721+TabawaX@users.noreply.github.com>
2024-06-17 19:15:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const cors = require("cors");
 const secure = require("ssl-express-www");
@@ -5,29 +6,27 @@ const path = require("path");
 
 const app = express();
 const __path = process.cwd();
-const statistik = '/public'
 
-
-// middleware
+// Middleware
 app.use(express.json());
 app.enable("trust proxy");
 app.set("json spaces", 2);
 app.use(cors());
 app.use(secure);
 
-app.use(express.static(statistik))
-
-
+// Middleware logging
 app.use((req, res, next) => {
   console.log(`Request URL: ${req.url}`);
   next();
 });
 
+// Path absolut ke folder 'public'
+app.use(express.static(path.join(__path, 'public')));
 
+// Routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__path, "sekai-page", "home.html"));
 });
-
 
 app.get("/docs", (req, res) => {
   res.sendFile(path.join(__path, "sekai-page", "docs.html"));
@@ -40,7 +39,7 @@ app.get("/api-docs", (req, res) => {
 const router = require('./api/router');
 app.use('/api', router);
 
-
+// Error handling
 app.use((req, res, next) => {
   res.status(404).json({
     message: 'Not Found'
