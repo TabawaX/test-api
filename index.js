7c41321dc6f6ccbf7ff93f926a1a2b96a6f6d5a1
Tabawa Renki <112997721+TabawaX@users.noreply.github.com>
2024-06-17 19:33:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,18 @@ const express = require("express");
 const cors = require("cors");
 const secure = require("ssl-express-www");
 const path = require("path");
-
 const app = express();
 const __path = process.cwd();
 
-// Middleware
+
 app.use(express.json());
 app.enable("trust proxy");
 app.set("json spaces", 2);
 app.use(cors());
 app.use(secure);
 
-// Middleware logging
-app.use((req, res, next) => {
-  console.log(`Request URL: ${req.url}`);
-  next();
-});
-
-// Path absolut ke folder 'public'
 app.use(express.static(path.join(__path, 'public')));
 
-// Routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__path, "sekai-page", "home.html"));
 });
@@ -39,17 +30,19 @@ app.get("/api-docs", (req, res) => {
 const router = require('./api/router');
 app.use('/api', router);
 
-// Error handling
+
 app.use((req, res, next) => {
-  res.status(404).json({
-    message: 'Not Found'
+  res.status(404).json({ 
+    engineer: "@Tabawa",
+    message: '404 Not Found Please Contact Developer'
   });
 });
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({
-    message: 'Internal Server Error'
+    engineer: "@Tabawa",
+    message: '500 Internal Server Error Please Contact Developer'
   });
 });
 
